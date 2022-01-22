64cf0d595b50011fc722b415f6f69a09c41b2e9f
TharunGoud1 <91940165+TharunGoud1@users.noreply.github.com>
2022-01-22 21:58:09 +0530

    test code

--- src/main/java/com/cedge/FirstProject/App.java
+++ src/main/java/com/cedge/FirstProject/App.java
@@ -6,8 +6,16 @@ package com.cedge.FirstProject;
  */
 public class App 
 {
+    int x=10;
+    int y=20;
+    public int add(int x,int y){
+        int z =x+y;
+       System.out.println( "Hello Add!"+z );
+    }
     public static void main( String[] args )
     {
+        add(10,15);
         System.out.println( "Hello World!" );
     }
+    
 }
