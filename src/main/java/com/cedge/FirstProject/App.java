acdfc03a53419f2c1faf28daa6c9b9d1d43255ff
TharunGoud1 <91940165+TharunGoud1@users.noreply.github.com>
2022-01-22 22:01:19 +0530

    ch

--- src/main/java/com/cedge/FirstProject/App.java
+++ src/main/java/com/cedge/FirstProject/App.java
@@ -9,12 +9,12 @@ public class App
     int x=10;
     int y=20;
     public static int add(int x,int y){
-        int z =x+y;
-       System.out.println( "Hello Add!"+z );
+       return int z =x+y;
     }
     public static void main( String[] args )
     {
-        add(10,15);
+        int a=add(10,15);
+        System.out.println( "Hello Add!"+a );
         System.out.println( "Hello World!" );
     }
     
